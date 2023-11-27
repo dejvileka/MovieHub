b06761a843bdf9b004c25ed2c5197658f1c77fb1
Dejvid Leka <deivi.leka@gmail.com>
2023-11-27 13:57:11 +0100

    not doing bullshit anymore. Im actually doing a login so this is great

--- app/src/main/java/com/dejvidleka/moviehub/ui/login/LogInFragment.kt
+++ app/src/main/java/com/dejvidleka/moviehub/ui/login/LogInFragment.kt
@@ -1,15 +1,18 @@
 package com.dejvidleka.moviehub.ui.login
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.dejvidleka.moviehub.R
+import com.dejvidleka.moviehub.databinding.FragmentLogInBinding
+import com.dejvidleka.moviehub.ui.MainActivity
 
 class LogInFragment : Fragment() {
 
+    private lateinit var binding: FragmentLogInBinding
     companion object {
         fun newInstance() = LogInFragment()
     }
@@ -25,7 +28,21 @@ class LogInFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_log_in, container, false)
+        binding = FragmentLogInBinding.inflate(LayoutInflater.from(context), container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        guestMode()
+    }
+
+    private fun guestMode() {
+        binding.guestButton.setOnClickListener {
+          val intent= Intent(context, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
