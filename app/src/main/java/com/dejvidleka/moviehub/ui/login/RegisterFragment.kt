e65c1fd0706591fbb91e650e62960dc9d64e8b57
Dejvid Leka <deivi.leka@gmail.com>
2023-12-05 14:32:22 +0100

    setting up sign up

--- app/src/main/java/com/dejvidleka/moviehub/ui/login/RegisterFragment.kt
+++ app/src/main/java/com/dejvidleka/moviehub/ui/login/RegisterFragment.kt
@@ -8,6 +8,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.dejvidleka.moviehub.R
 import com.dejvidleka.moviehub.databinding.FragmentRegisterBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -35,41 +37,33 @@ class RegisterFragment : Fragment() {
 
 
         val db = FirebaseFirestore.getInstance()
-        val user = hashMapOf(
-            "first" to "Ada",
-            "last" to "Lovelace",
-            "born" to 1815
-        )
-        db.collection("users")
-            .add(user)
-            .addOnSuccessListener { documentReference ->
-                Log.d("added in db", "DocumentSnapshot added with ID: ${documentReference.id}")
-            }
-            .addOnFailureListener { e ->
-                Log.w("not added in db", "Error adding document", e)
-            }
 
         val buttonSubmit = binding.logInButton
         buttonSubmit.setOnClickListener {
             val name = binding.firstnameInputEditText.text.toString()
             val lastName = binding.lastnameInputEditText.text.toString()
-            val age= binding.ageInputEditText.text.toString()
-            val password= binding.passwordInputEditText.text.toString()
+            val age = binding.ageInputEditText.text.toString()
+            val password = binding.passwordInputEditText.text.toString()
+            val email = binding.emailInputEditText.text.toString()
 
-            val user = hashMapOf(
-                "name" to name,
-                "last_name" to lastName,
-                "password" to password,
-                "age" to age
-            )
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
 
-            db.collection("users")
-                .add(user)
-                .addOnSuccessListener { documentReference ->
-                    Log.d("added in db", "DocumentSnapshot added with ID: ${documentReference.id}")
-                }
-                .addOnFailureListener { e ->
-                    Log.w("not added in db", "Error adding document", e)
+                        val firebaseUser = it.result!!.user!!
+                        val userId = firebaseUser.uid
+                        val userMap = hashMapOf(
+                            "name" to name,
+                            "last_name" to lastName,
+                            "age" to age,
+                        )
+
+                        db.collection("users").document(userId).set(userMap)
+                            .addOnSuccessListener { documentReference ->
+                            }
+                            .addOnFailureListener { e ->
+                            }
+                    }
                 }
         }
     }
