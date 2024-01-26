2e7c1aa496e4da80e25d874bb2cc11838295e9ac
Dejvid Leka <deivi.leka@gmail.com>
2024-01-26 15:55:18 +0100

    making functionality out of auth

--- app/src/main/java/com/dejvidleka/moviehub/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/dejvidleka/moviehub/ui/favorites/FavoritesFragment.kt
@@ -1,17 +1,22 @@
 package com.dejvidleka.moviehub.ui.favorites
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.SeekBar
+import android.widget.Spinner
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.dejvidleka.data.local.models.Genre
 import com.dejvidleka.data.local.models.MovieResult
+import com.dejvidleka.moviehub.R
 import com.dejvidleka.moviehub.databinding.FragmentFavoritesBinding
 import com.dejvidleka.moviehub.domain.Result
 import com.dejvidleka.moviehub.ui.adapters.FavoriteMovieAdapter
@@ -53,12 +58,56 @@ class FavoritesFragment : Fragment(), MovieClickListener {
                 }
             }
         }
+        val spinnerYear = binding.spinnerYear
+        setupYearSpinner(this.requireContext(), spinnerYear)
+
     }
+
 
     override fun onMovieClick(movieResult: MovieResult, view: View) {
         val directions =
             FavoritesFragmentDirections.actionFavoritesFragmentToMovieDetailFragment(movieResult)
         view.findNavController().navigate(directions)
+    }
+
+
+    private fun setupYearSpinner(context: Context, spinner: Spinner) {
+        ArrayAdapter.createFromResource(
+            context,
+            R.array.years_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+            }
+        }
+        val seekBarAgeRating = binding.seekbarAgeRating
+        val textViewSelectedAge = binding.textviewSelectedAge
+
+        seekBarAgeRating.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textViewSelectedAge.text = "$progress years"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
     }
 
     override fun onViewMoreClick(genre: Genre, view: View) {
